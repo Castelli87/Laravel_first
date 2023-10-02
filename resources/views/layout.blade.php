19985a28e4d26d8f4ffdf72ea4a809a5dfa08717
Castelli87 <davidecastelli87@hotmail.com>
2023-10-02 10:33:13 +0100

    add a javascript code that increment the quantity of the pizza, i dont know if gonna works :D '

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -47,11 +47,8 @@
 >
     <p class="ml-2">Copyright &copy; 2023, All Rights reserved</p>
 
-    {{-- <a
-        href="create.html"
-        class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
-        >Post Job</a
-    > --}}
+    @yield('footer-scripts')
 </footer>
+
 </body>
 </html>
--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -2,20 +2,14 @@
 
 @section('content')
     @include('partials._search')
-    {{-- <h2 class="">{{ $pizza['name'] }}</h2>
 
-    <p>{{ $pizza['toppings'] }}</p>
-
-    <p>{{ $pizza['price'] }}</p>
-
-    <p>{{ $pizza['preparation_time'] }} mins</p> --}}
     <div class="mx-4">
         <div class="bg-gray-50 border border-gray-200 p-10 rounded">
             <div class="flex flex-col items-center justify-center text-center">
                 <img class="w-49 mr-6 mb-6" src="{{ asset('images/') . '/' . $pizza->name . '.jpg' }}" alt="" />
 
                 <h3 class="text-2xl mb-2">{{ $pizza->name }}</h3>
-                <div class="text-xl font-bold mb-4">{{$pizza->category}}</div>
+                <div class="text-xl font-bold mb-4">{{ $pizza->category }}</div>
                 <ul class="flex m-3">
 
                     <x-pizza-toppings :toppings='$pizza->toppings' />
@@ -29,18 +23,48 @@
                     <div class="text-lg space-y-6">
                         <p>Preparation time: {{ $pizza->preparation_time }} mins</p>
 
-                        <div class="flex  justify-center items-center space-x-7">
-                            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM184 232H328c13.3 0 24 10.7 24 24s-10.7 24-24 24H184c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
-                            <p class="text-2xl font-bold">1</p>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/></svg>
+                        <div class="flex justify-center items-center space-x-7">
+                            <i class="fa-solid fa-circle-minus" id="decrease"></i>
+                            <p class="text-2xl font-bold" id="number">0</p>
+                            <i class="fa-solid fa-circle-plus" id="increase"></i>
                         </div>
 
-
-                        <a href='#'' class="block bg-laravel text-white mt-6  p-3 py-2 rounded-xl hover:opacity-80">
+                        <a href='#' class="block bg-laravel text-white mt-6  p-3 py-2 rounded-xl hover:opacity-80">
                             ORDER THIS PIZZA</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('footer-scripts')
+    <script>
+        // Get references to the plus and minus icons and the number element
+        const increaseButton = document.getElementById('increase');
+        const decreaseButton = document.getElementById('decrease');
+        const numberElement = document.getElementById('number');
+
+        // Initialize a variable to store the current number
+        let currentNumber = 0;
+
+        // Function to update the number element
+        function updateNumber() {
+            numberElement.textContent = currentNumber;
+        }
+
+        // Event listener for the plus button
+        increaseButton.addEventListener('click', () => {
+            currentNumber++; // Increment the number
+            updateNumber(); // Update the displayed number
+        });
+
+        // Event listener for the minus button
+        decreaseButton.addEventListener('click', () => {
+            if (currentNumber > 0) {
+                currentNumber--; // Decrement the number (if greater than 1)
+                updateNumber(); // Update the displayed number
+            }
+        });
+    </script>
 @endsection
