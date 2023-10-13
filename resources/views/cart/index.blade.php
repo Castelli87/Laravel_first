32f26a9b6c2cee468028065cd8e0cd4469eb247f
Castelli87 <davidecastelli87@hotmail.com>
2023-10-13 10:33:48 +0100

    Almost finsih the cart index in a dynamic way

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -13,34 +13,40 @@
                 <div class="w-1/6">Total</div>
                 <div class="w-1/6 text-right">Clear All</div>
             </div>
+            @foreach (Cart::content() as $pizza)
+                <div class="flex flex-col mx-auto md:flex-row items-center border-b-2 border-gray-300 py-5 mb-3 text-xl">
+                    <div class="mb-3 md:w-1/6 mb-2 md:mb-0">
+                        <img src="{{ asset('images/') . '/' . $pizza->name . '.jpg' }}" alt="Product Image"
+                            class="w-40 rounded-md">
+                    </div>
+                    <div class="mb-3 md:w-2/6 md:pl-2">
+                        <p class="text-extrabold text-2xl">{{ $pizza->name }}</p>
+                    </div>
+                    <div class="mb-3 md:w-1/6 md:pl-4">
+                        <p>£{{ $pizza->price }}</p>
+                    </div>
+                    <div class="mb-3 md:w-1/6 md:pl-4">
+                        <div class="flex items-center">
+                            <button type="button"><i class="fa-solid fa-circle-minus text-xl " id="increase"></i></button>
+                            <input type="text" value="{{$pizza->qty}}"class="h-10 w-10 text-center text-2xl font-bold mx-2"
+                                id="number"">
+                            <button type="button"><i class="fa-solid fa-circle-plus text-xl" id="increase"></i></button>
+                        </div>
+                    </div>
+                    <div class="mb-3 md:w-1/6 md:pl-4">
+                        <p>£{{ $pizza->weight }}</p>
+                    </div>
+                    <div class="md:w-1/6 md:pl-4 text-right">
+                        <button
+                            class="w-8 h-8 border rounded-md text-white font-bold bg-laravel hover:bg-red-700">X</button>
 
-            <!-- Sample cart item -->
-            <div class="flex flex-col mx-auto md:flex-row items-center border-b-2 border-gray-300 py-5 mb-3 text-xl">
-                <div class="mb-3 md:w-1/6 mb-2 md:mb-0">
-                    <img src="/images/Margherita.jpg" alt="Product Image" class="w-40 rounded-md">
-                </div>
-                <div class="mb-3 md:w-2/6 md:pl-2">
-                    <p class="text-extrabold text-2xl">Margherita</p>
-                </div>
-                <div class="mb-3 md:w-1/6 md:pl-4">
-                    <p>£9.99</p>
-                </div>
-                <div class="mb-3 md:w-1/6 md:pl-4">
-                    <div class="flex items-center">
-                        <button type="button"><i class="fa-solid fa-circle-minus text-xl " id="increase"></i></button>
-                        <input type="text" value="1"class="h-10 w-10 text-center text-2xl font-bold mx-2" id="number"">
-                        <button type="button"><i class="fa-solid fa-circle-plus text-xl" id="increase"></i></button>
                     </div>
                 </div>
-                <div class="mb-3 md:w-1/6 md:pl-4">
-                    <p>$9.99</p>
-                </div>
-                <div class="md:w-1/6 md:pl-4 text-right">
-                    <button class="w-8 h-8 border rounded-md text-white font-bold bg-laravel hover:bg-red-700">X</button>
+            @endforeach
+            @if (Cart::content()->count() === 0)
+                <h2 class="text-3xl text-center font-semibold mt-12 mb-4 ">Cart is empty</h2>
+            @endif
 
-                </div>
-            </div>
-           
 
         </div>
     </div>
